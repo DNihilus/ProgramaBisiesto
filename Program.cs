f4521a2a0f3a3df960c0912ad7c5f66570cb33e7
DNihilus <67303504+DNihilus@users.noreply.github.com>
2020-10-21 02:44:47 -0500

    Actualización
    Actualizando el programa para mostrar mensajes en color y una mejor funcionalidad

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-﻿using System;
+/*
+ * Programa que determina si un año ingresado
+ * en valor numérico enteroy positivo
+ * es o no un año bisiesto
+ * 
+ * Creado    : 14/10/2020
+ * Modificado: 21/10/2020
+ * */
+using System;
 
 namespace bisiesto
 {
 	class Program
 	{
+		
+		/*
+		 * Función que decide si un número entero es o no positivo
+		 * Recibe:
+		 *  int numero : Número a revisar
+		 * 
+		 * Regresa:
+		 *  bool valor : Un valor booleano que indica si el número recibido es o no positivo
+		 * */
+		public static bool revisarIntPositivo(int numero)
+		{
+			return numero >= 0;
+		}
 		
 		/* Función de lectura para asegurar la conversión a entero
 		 * Recibe:
@@ -19,24 +40,55 @@ namespace bisiesto
 			// Se coloca todo dentro de un ciclo infinito
 			while(true)
 			{
+				// Limpiar la consola
+				Console.Clear();
+				// Colocar la cadena de caracteres en posicion
+				Console.SetCursorPosition(0,0);
 				// Se recibe una cadena de caracteres
-				Console.WriteLine(mensaje);
+				Console.Write(mensaje);
 				resp = Console.ReadLine();
 				// Se ingresa a una sección try - catch
 				try
 				{
-					// En este caso se intenará realizar una conversión a un calor entero 
+					// En este caso se intenará realizar una conversión a un valor entero 
 					valor = Convert.ToInt32(resp);
-					// En caso de que se logre romperá el ciclo
-					break;
+					// Una vez que no aparezcan errores, se procede a verificar si el número
+					// es positivo
+					if(revisarIntPositivo(valor)){
+						// FIXME: Modificar para que esto sólo se muestre con una función
+						Console.ForegroundColor = ConsoleColor.Green;
+						Console.SetCursorPosition(0,1);
+						Console.WriteLine("Valor correcto");
+						Console.ForegroundColor = ConsoleColor.Gray;
+						// En caso de que se logre romperá el ciclo
+						break;
+					}
+					else
+					{
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.SetCursorPosition(0,2);
+						Console.WriteLine("El valor no es positivo");
+						Console.SetCursorPosition(0,3);
+						Console.WriteLine("¡Favor de ingresar de nuevo!");
+						Console.ForegroundColor = ConsoleColor.Gray;						
+					}
 				}
 				// Aquí se maneja la excepción, esta es del tipo FormatException
 				catch(FormatException e)
 				{
-					// Mostrar mensajes de error
-					Console.WriteLine(e);
+					// Cambiar a color rojo
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.SetCursorPosition(0,2);
 					Console.WriteLine("El valor no es un número entero");
+					Console.SetCursorPosition(0,3);
 					Console.WriteLine("¡Favor de ingresar de nuevo!");
+					// Regresar al color original
+					Console.ForegroundColor = ConsoleColor.Gray;
+				}
+				finally
+				{
+					// FIXME: Hacer que esto solo aparezca cuando hay algún error
+					Console.ReadKey(true);
 				}
 			}
 			// Una vez que se logre salir del ciclo, se regresa el valor
@@ -45,28 +97,24 @@ namespace bisiesto
 		
 		public static void Main(string[] args)
 		{
-			// TODO: Colocar colores y esas cosas en el programa
 			int aa;			
-			/*do
-			{
-				Console.WriteLine("Ingresar un año (valor entero y positivo): ");
-				string a = Console.ReadLine();
-				// Así ya se puede convertir a un entero de 32 bits
-				aa = Convert.ToInt32(a);
-				if (aa > 0)
-					break;
-				else
-					Console.WriteLine("No manches, que no sabes leer!");
-			}
-			while(true);*/
-			aa = ingresarInt("Ingresar un año (valor entero y positivo): ");
+			
+			// Inicia el nombre de la ventana
+			Console.Title = "Año bisiesto";
+			
+			//aa = ingresarInt("Ingresar un año (valor entero y positivo): ");
+			aa = ingresarInt();
 			
 			// Probación de que es un año bisiesto
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.SetCursorPosition(0,2);
 			if( (aa % 4 == 0)&&(aa % 100 != 0)||(aa % 400 == 0) )
 				Console.WriteLine("El año {0} es bisiesto", aa);
 			else
 				Console.WriteLine("El año {0} NO es bisiesto", aa);
+			Console.ForegroundColor = ConsoleColor.Gray;
 			
+			Console.SetCursorPosition(0,5);
 			Console.Write("Presione una tecla para continuar . . . ");
 			Console.ReadKey(true);
 		}
